4d2d29976ddc8b772018d435486b36b3ec652b31
GamingCoder <coreoder@gmail.comm>
2014-03-15 18:21:06 +0100

    Add README.md and some functions

---
+++ README.md
@@ -0,0 +1,15 @@
+# dev-null-logger
+Logger that as silent as /dev/null
+
+## Functions
+```js
+var logger = require('dev-null-logger');
+
+logger.log('something');
+logger.info('something');
+logger.warn('something');
+logger.error('something');
+logger.debug('something');
+loger.dir({});
+logger.trace();
+```
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ Logger.prototype.log = function (val) {};
 Logger.prototype.info = function (val) {};
 Logger.prototype.warn = function (val) {};
 Logger.prototype.error = function (val) {};
-Logger.prototype.trace = function (val) {};
+Logger.prototype.trace = function () {};
+Logger.prototype.debug = function (val) {};
+Logger.prototype.dir = function (val) {};
 
 exports = module.exports = new Logger();
